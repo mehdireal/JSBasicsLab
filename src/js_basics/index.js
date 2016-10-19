518ff8731dc9a4d715b2cc057ba88b03e15a3f57
mehdireal <mohamedelmehditioutchi@gmail.com>
2016-10-19 14:00:43 +0200

    TP1_JS_Mohamed_El_Mehdi_TIOUITCHI

--- src/js_basics/index.js
+++ src/js_basics/index.js
@@ -2,30 +2,69 @@
 export const isNumberEven = (i) => {
   // i will be an integer.
   // Return true if it's even, and false if it isn't.
+if ((i & 1) === 0) {
+    return true;
+} else {
+    return false;
+}
 };
 
 export const getFileExtension = (str) => {
-  // str will be a string, but it may not have a file extension.
-  // Return the file extension (with no period) if it has one, otherwise false
+
+
+  if (str.lastIndexOf(".") > 0) {
+    var str1 = str.substr(str.lastIndexOf("."));
+    var str2 = str1.split('.');
+    return str2[1];
+} else {
+    return false;
+}
+ 
 };
 
 export const longestString = (arr) => {
-  // arr will be an array.
-  // Return the longest string in the array
+var res="";
+var i;
+    
+    for (i=0; i<arr.length;i++){
+    
+        if (arr[i].length> res.length  && typeof arr[i]==='string')
+	
+	   res = arr[i];
+    }
+ 
+return res;
 };
 
 export const reverseString = (str) => {
-  // str will be an string
-  // Return a new string who's characters are in the opposite order to str's.
+
+  
+String.prototype.reverse = function ()
+{
+        var n   =  '';
+        for( var i=this.length-1; i >= 0; i--)
+                n       +=     this.charAt(i);
+        return n;
+}
+return str.reverse();
 };
 
 export const isPalindrome = (str) => {
-  // str will be an string
-  // Return true if it is a palindrome and false otherwise.
-  // It should be case insensitive and not consider space or punctuation.
+
+
+  if(str.toLowerCase() === reverseString(str.toLowerCase()))
+  return true;
+  else return false;
 };
 
 export const nestedSum = (arr) => {
-  // arr will be an array, containing integers, strings and/or arrays like itself
-  // Return the sum all the numbers you find, anywhere in the nest of arrays.
+
+var i, sum =0;
+    for (i=0;i<arr.length;i++){
+	if (Array.isArray(arr[i])){
+		sum+=nestedSum(arr[i]);
+        }else if(Number.isInteger(arr[i]))
+	   sum+=arr[i];
+    }
+return sum;
 };
